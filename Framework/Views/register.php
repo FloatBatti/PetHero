7d2b1cbf760655f22f7a33dea17eccbdb1476bad
isCuyo <114100273+isCuyo@users.noreply.github.com>
2022-10-08 16:14:55 -0300

    creacion de usuario

---
+++ Framework/Views/register.php
@@ -0,0 +1,26 @@
+<div class="">
+<form action="" method="post">
+
+    <label for="usuario">Usuario</label>
+        <input type="text" name="" class="" required>
+
+    <label for="nombre">Nombre</label>
+        <input type="text" name="" class="" required>
+
+        <label for="apellido">Apellido</label>
+            <input type="text" name="" class="" required>
+        
+        <label for="dni">DNI</label>
+            <input type="text" name="" class="" required>
+
+            <label for="mail">Correo Electronico</label>
+        <input type="text" name="" class="" required>
+
+        <label for="pass">Contraseña</label>
+        <input type="password" name="" class="" required>
+
+        <label for="=re-pass">Contraseña</label>
+        <input type="password" name="" class="" required>
+
+</form>
+</div>
