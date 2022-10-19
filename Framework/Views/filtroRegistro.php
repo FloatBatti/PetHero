53b652dedd096b35f3cbee24924fc0373a23a5d1
isCuyo <114100273+isCuyo@users.noreply.github.com>
2022-10-19 12:09:51 -0300

    update front registro filtro

--- Framework/Views/filtroRegistro.php
+++ Framework/Views/filtroRegistro.php
@@ -10,28 +10,56 @@
    
   </head>
   <body>
-    <div class="logo contorno">PET HERO</div>
-    <div class="contenedora-registro-filtro contorno">
-        <div class="contenedora-eleccion dueño">
-            <div class="interno contorno">
-                <div class="titulo contorno"><h2>Dueño</h2></div>
-                <div class="contorno"><p>Deberas brindar datos personales y de contacto. Podras registrar las mascotas que desees. Una vez hecho ya podras usar los servicios de la web.</p></div>
-                <div class="contorno">DIBUJO IMG</div>
-                <div class="contorno"><a href="">Registrarme</a></div>
-            </div>
+    <div class="cabecera"><!--contenedora para el logo en el top + faqs, etc -->  
+      <img src="../img/PetHeroLogo.png">
+    </div>
+
+    <div class="contenedora-gral"><!--aca esta el contenedor q contiene las 2 opciones -->
+
+      <div class="contenedora-eleccion dueño"><!--contenedora para eleccion de dueño -->
+       
+        <div class="contenedora-titulo">
+          <img src="../img/icono-Dueño.png" height="400" widht="450">
+        </div>
+        
+        <div class="contenedora-texto">
+          <div class="texto">
+              <ul>
+                <li>Deberás brindar datos personales y de contacto.</li>
+                <li>Podrás registrar las mascotas que desees.</li>
+                <li>Una vez hecho ya podras usar los servicios de la web..</li>
+            </ul>
+          </div>
+        </div>
+        
+        <div class="contenedora-boton">
+        <a href="<?php echo FRONT_ROOT . "Duenos/RegisterView"?>"><img src="../img/RegistrarmeResized.png" height="70"></a>
         </div>
 
-        <div class="contenedora-eleccion guardian">
-            <div class="interno contorno">
-                <div class="titulo contorno"><h2>Guardian</h2></div>
-                <div class="contorno"><p>Deberas brindar datos personales y de contacto. Sera necesario agregar informacion acerca del espacio brindado para el cuidado de las mascotas como asi mismo una descripcion del servicio ofrecido y registrar la disponibilidad con la que lo haras.</p></div>            
-                <div class="contorno">DIBUJO IMG</div>
-                <div class="contorno"><a href="<?php echo FRONT_ROOT . "Guardianes/FirstRegisterView"?>">Registrarme</a> </div>   
-            </div>   
+      </div>
+
+      <div class="contenedora-eleccion guardian"><!--acontenedora para eleccion de guardian -->
+      
+        <div class="contenedora-titulo">
+        <img src="../img/My Project-1.png" >
         </div>
+
+        <div class="contenedora-texto">
+          <div class="texto">Deberas brindar datos personales y de contacto.<br> Será necesario agregar informacion acerca del espacio brindado<br> para el cuidado de las mascotas como asi mismo <br>una descripcion del servicio ofrecido <br>y registrar la disponibilidad con la que lo haras</div>
+        </div>
+
+        <div class="contenedora-boton">
+        <a href="<?php echo FRONT_ROOT . "Guardianes/FirstRegisterView"?>"><img src="../img/RegistrarmeResized.png" height="70"></a>
+      </div>
+
+      </div>
     </div>
-    <div id="separador" class="contorno">ACA va el footer</div>
-  </body>
+    <div class="top-footer"></div><!--ofrece un margen inferior por encima del footer -->
+
+    
 <footer>
+<div>Copyright &#169 2022 Pet Hero S.A. es una empresa del grupo Batti's System CO.</div>
+<div><a href="">Terminos y Condiciones</a></div>
+<div><a href="">Aviso de privacidad</a></div>
 </footer>
 </html>
