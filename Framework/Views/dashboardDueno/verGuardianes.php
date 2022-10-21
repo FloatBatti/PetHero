b2b5f27fd766909caeeaf80a3bb243c5164337ee
isCuyo <114100273+isCuyo@users.noreply.github.com>
2022-10-20 23:17:36 -0300

    vista regGuardian

--- Framework/Views/dashboardDueno/verGuardianes.php
+++ Framework/Views/dashboardDueno/verGuardianes.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ver Guardianes</title>
     
-    <link href="../../styles/dashboardDueño.css" rel="stylesheet" >
-    <link href="../../styles/verGuardianes.css" rel="stylesheet" >
+    <link href="../../styles/dashboardDueño.css" rel="stylesheet">
+    <link href="../../styles/verGuardianes.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
