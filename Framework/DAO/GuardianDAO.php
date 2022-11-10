6a72c2fc6adeeaf40e2df56c697b82f165c326d7
Agustin Battisti <agusbatti52@gmail.com>
2022-11-10 12:28:05 -0300

    Refactor del codigo post correciones

--- Framework/DAO/GuardianDAO.php
+++ Framework/DAO/GuardianDAO.php
@@ -15,35 +15,35 @@ class GuardianDAO implements InterfaceDAO
 
     public function GetAll()
     {
+        $guardianesList = array();
+
+        $query = "SELECT 
+        g.id_usuario,
+        u.username,
+        u.nombre,
+        u.apellido,
+        u.correo,
+        u.telefono,
+        u.foto_perfil,
+        g.dia_inicio,
+        g.dia_fin,
+        g.descripcion,
+        g.costo_diario,
+        g.foto_espacio
+        FROM
+        usuarios u
+        INNER JOIN
+        guardianes g ON g.id_usuario = u.id_usuario";
+
+
+        $this->connection = Connection::GetInstance();
 
         try {
-
-            $guardianesList = array();
-
-            $query = "SELECT 
-            g.id_usuario,
-            u.username,
-            u.nombre,
-            u.apellido,
-            u.correo,
-            u.telefono,
-            u.foto_perfil,
-            g.dia_inicio,
-            g.dia_fin,
-            g.descripcion,
-            g.costo_diario,
-            g.foto_espacio
-            FROM
-            usuarios u
-            INNER JOIN
-            guardianes g ON g.id_usuario = u.id_usuario";
-
-
-            $this->connection = Connection::GetInstance();
 
             $resultSet = $this->connection->Execute($query);
 
             foreach($resultSet as $reg){
+
                 $guardian=new Guardian();
                 $guardian->setId($reg["id_usuario"]);
                 $guardian->setUsername($reg["username"]);
@@ -70,36 +70,37 @@ class GuardianDAO implements InterfaceDAO
     }
     public function GetFavoritos()
     {
+        $listaFavoritos = array();
+
+        $query = "SELECT
+        g.id_guardian, 
+        g.id_usuario,
+        u.username,
+        u.nombre,
+        u.apellido,
+        u.correo,
+        u.telefono,
+        u.foto_perfil,
+        g.dia_inicio,
+        g.dia_fin,
+        g.descripcion,
+        g.costo_diario,
+        g.foto_espacio
+        from favoritos f
+        inner join guardianes g on f.id_guardianFav=g.id_guardian
+        inner join usuarios u on g.id_usuario=u.id_usuario
+        inner join dueños d on f.id_dueño=d.id_dueño";
+
+
+        $this->connection = Connection::GetInstance();
+
 
         try {
-
-            $listaFavoritos = array();
-
-            $query = "SELECT
-            g.id_guardian, 
-            g.id_usuario,
-            u.username,
-            u.nombre,
-            u.apellido,
-            u.correo,
-            u.telefono,
-            u.foto_perfil,
-            g.dia_inicio,
-            g.dia_fin,
-            g.descripcion,
-            g.costo_diario,
-            g.foto_espacio
-            from favoritos f
-            inner join guardianes g on f.id_guardianFav=g.id_guardian
-            inner join usuarios u on g.id_usuario=u.id_usuario
-            inner join dueños d on f.id_dueño=d.id_dueño";
-
-
-            $this->connection = Connection::GetInstance();
 
             $resultSet = $this->connection->Execute($query);
 
             foreach($resultSet as $reg){
+
                 $guardian=new Guardian();
                 $guardian->setId($reg["id_usuario"]);
                 $guardian->setUsername($reg["username"]);
@@ -127,21 +128,22 @@ class GuardianDAO implements InterfaceDAO
 
     public function Add($guardian)
     {
+        $query = "INSERT INTO 
+        guardianes (id_usuario, dia_inicio, dia_fin, descripcion, costo_diario, foto_espacio) 
+        values ((select id_usuario from usuarios where username = :username), 
+        :dia_inicio, :dia_fin, :descripcion, :costo_diario, :foto_espacio)";
+
+        $parameters["username"] = $guardian->getUsername();
+        $parameters["dia_inicio"] = $guardian->getFechaInicio();
+        $parameters["dia_fin"] = $guardian->getFechaFin();
+        $parameters["descripcion"] = $guardian->getDescripcion();
+        $parameters["costo_diario"] = $guardian->getCosto();
+        $parameters["foto_espacio"] = $guardian->getFotoEspacioURL();
+
+        $this->connection = Connection::GetInstance();
 
         try {
 
-            $query = "INSERT INTO 
-                guardianes (id_usuario, dia_inicio, dia_fin, descripcion, costo_diario, foto_espacio) 
-                values ((select id_usuario from usuarios where username = '" . $guardian->getUsername() . "'), 
-                :dia_inicio, :dia_fin, :descripcion, :costo_diario, :foto_espacio)";
-
-            $parameters["dia_inicio"] = $guardian->getFechaInicio();
-            $parameters["dia_fin"] = $guardian->getFechaFin();
-            $parameters["descripcion"] = $guardian->getDescripcion();
-            $parameters["costo_diario"] = $guardian->getCosto();
-            $parameters["foto_espacio"] = $guardian->getFotoEspacioURL();
-
-            $this->connection = Connection::GetInstance();
 
             $this->connection->ExecuteNonQuery($query, $parameters);
 
@@ -149,11 +151,14 @@ class GuardianDAO implements InterfaceDAO
 
     
                 $query = "INSERT INTO tamaños_x_guardianes(id_tamaño, id_guardian) 
-                    VALUES ((select id_tamaño from tamaños where nombre_tamaño = '" . $tamaño . "'), 
-                    (select id_guardian from guardianes g inner join usuarios u on u.id_usuario = g.id_usuario where u.username = '" . $guardian->getUsername() . "'));";
+                    VALUES ((select id_tamaño from tamaños where nombre_tamaño = :tamano), 
+                    (select g.id_guardian from guardianes g inner join usuarios u on u.id_usuario = g.id_usuario where u.username = :username));";
+
+                $parametersTam["tamano"] = $tamaño;
+                $parametersTam["username"] = $guardian->getUsername();
 
 
-                $resultado = $this->connection->ExecuteNonQuery($query);
+                $resultado = $this->connection->ExecuteNonQuery($query, $parametersTam);
             }
 
 
@@ -167,40 +172,44 @@ class GuardianDAO implements InterfaceDAO
     public function devolverGuardianPorId($idUsuario){
         
         $guardian = new Guardian();
-        try{
-            $query = "SELECT 
+
+        $query = "SELECT 
         * 
         FROM usuarios u 
         inner join guardianes g 
         on u.id_usuario = g.id_usuario
-        where u.id_usuario = " . $idUsuario . ";";
+        where u.id_usuario = :id_usuario;";
+
+        $parameters["id_usuario"] = $idUsuario;
 
         $this->connection = Connection::GetInstance();
 
-        $resultSet = $this->connection->Execute($query);
-
+        try{
         
-        foreach($resultSet as $reg){
+            $resultSet = $this->connection->Execute($query, $parameters);
 
-            $guardian->setId($reg["id_usuario"]);
-            $guardian->setUsername($reg["username"]);
-            $guardian->setDni($reg["dni"]);
-            $guardian->setNombre($reg["nombre"]);
-            $guardian->setApellido($reg["apellido"]);
-            $guardian->setCorreoelectronico($reg["correo"]);
-            $guardian->setPassword($reg["password"]);
-            $guardian->setTelefono($reg["telefono"]);
-            $guardian->setDireccion($reg["direccion"]);
-            $guardian->setFotoPerfil($reg["foto_perfil"]);
-            $guardian->setTipoUsuario($reg["tipo_usuario"]);
-            $guardian->setFechaInicio($reg["dia_inicio"]);
-            $guardian->setFechaFin($reg["dia_fin"]);
-            $guardian->setDescripcion($reg["descripcion"]);
-            $guardian->setCosto($reg["costo_diario"]);
-            $guardian->setFotoEspacioURL($reg["foto_espacio"]);
-            $guardian->setTipoMascota($this->obtenerTamañosMascotas($reg["id_usuario"]));
             
-            return $guardian;
+            foreach($resultSet as $reg){
+
+                $guardian->setId($reg["id_usuario"]);
+                $guardian->setUsername($reg["username"]);
+                $guardian->setDni($reg["dni"]);
+                $guardian->setNombre($reg["nombre"]);
+                $guardian->setApellido($reg["apellido"]);
+                $guardian->setCorreoelectronico($reg["correo"]);
+                $guardian->setPassword($reg["password"]);
+                $guardian->setTelefono($reg["telefono"]);
+                $guardian->setDireccion($reg["direccion"]);
+                $guardian->setFotoPerfil($reg["foto_perfil"]);
+                $guardian->setTipoUsuario($reg["tipo_usuario"]);
+                $guardian->setFechaInicio($reg["dia_inicio"]);
+                $guardian->setFechaFin($reg["dia_fin"]);
+                $guardian->setDescripcion($reg["descripcion"]);
+                $guardian->setCosto($reg["costo_diario"]);
+                $guardian->setFotoEspacioURL($reg["foto_espacio"]);
+                $guardian->setTipoMascota($this->obtenerTamañosMascotas($reg["id_usuario"]));
+                
+                return $guardian;
         }
         }catch (Exception $ex) {
             throw $ex;
@@ -221,53 +230,72 @@ class GuardianDAO implements InterfaceDAO
         guardianes g ON txg.id_guardian = g.id_guardian
         INNER JOIN
         usuarios u on u.id_usuario = g.id_usuario
-        WHERE u.id_usuario=".$idUsuario.";";
+        WHERE u.id_usuario= :id_usuario;";
+
+        $parameters["id_usuario"] = $idUsuario;
           
 
         $this->connection = Connection::GetInstance();
 
-        $resultSet = $this->connection->Execute($query);
+        try{
 
-        foreach ($resultSet as $reg){
+            $resultSet = $this->connection->Execute($query, $parameters);
 
-            array_push($listaTamaños, $reg["nombre_tamaño"]);
+            foreach ($resultSet as $reg){
+    
+                array_push($listaTamaños, $reg["nombre_tamaño"]);
+    
+            }
+    
+            return $listaTamaños;
+
 
         }
+        catch(Exception $ex){
 
-        return $listaTamaños;
-
+            throw $ex;
+        }
 
     }
     public function grabarDisponibilidad($fechaInicio,$fechaFin,$sizes,$costo,$fotoUrl,$descripcion){
+
             $resultado=null;
+
+            $queryUpdate = "UPDATE guardianes g
+            set g.dia_inicio = :fechaInicio, g.dia_fin=:fechaFin, g.costo_diario=:costo,g.foto_espacio=:fotoUrl, g.descripcion=:descripcion WHERE g.id_usuario=:buscado";
+
+            $parameters["fechaInicio"] = $fechaInicio;
+            $parameters["fechaFin"] = $fechaFin;
+            $parameters["costo"] = $costo;
+            $parameters["fotoUrl"] = $fotoUrl;
+            $parameters["descripcion"] = $descripcion;
+            $parameters["buscado"] = ($_SESSION["UserId"]);
+
+            $queryDelete = "DELETE from tamaños_x_guardianes
+            where id_guardian = (select g.id_guardian from guardianes g inner join usuarios u on u.id_usuario = g.id_usuario where g.id_usuario= :id_usuario );";
+
+            $parametersDel["id_usuario"] = $_SESSION["UserId"];
+
+           $this->connection = Connection::GetInstance();
+
         try{
-            $query = "UPDATE guardianes g
-                set g.dia_inicio = :fechaInicio, g.dia_fin=:fechaFin, g.costo_diario=:costo,g.foto_espacio=:fotoUrl, g.descripcion=:descripcion WHERE g.id_usuario=:buscado";
 
-               $parameters["fechaInicio"] = $fechaInicio;
-               $parameters["fechaFin"] = $fechaFin;
-               $parameters["costo"] = $costo;
-               $parameters["fotoUrl"] = $fotoUrl;
-               $parameters["descripcion"] = $descripcion;
-               $parameters["buscado"] = ($_SESSION["UserId"]);
-
-
-               $this->connection = Connection::GetInstance();
-               $this->connection->ExecuteNonQuery($query, $parameters);
-
-                $query = "DELETE from tamaños_x_guardianes
-                where id_guardian = (select g.id_guardian from guardianes g inner join usuarios u on u.id_usuario = g.id_usuario where g.id_usuario= " . $_SESSION["UserId"] . ");";
+                $this->connection->ExecuteNonQuery($queryUpdate, $parameters);
                                    
-                $this->connection->ExecuteNonQuery($query);
+                $this->connection->ExecuteNonQuery($queryDelete, $parametersDel);
 
 
-               foreach ($sizes as $tamano) {
+               foreach ($sizes as $tamaño) {
         
-                $query = "INSERT INTO tamaños_x_guardianes(id_tamaño, id_guardian) 
-                    VALUES ((select id_tamaño from tamaños where nombre_tamaño = '" . $tamano . "'), 
-                    (select id_guardian from guardianes g inner join usuarios u on u.id_usuario = g.id_usuario where g.id_usuario= " . $_SESSION["UserId"] . "));";
+                    $query = "INSERT INTO tamaños_x_guardianes(id_tamaño, id_guardian) 
+                        VALUES ((select id_tamaño from tamaños where nombre_tamaño = :tamano), 
+                        (select id_guardian from guardianes g inner join usuarios u on u.id_usuario = g.id_usuario where g.id_usuario= :id_usuario));";
 
-                $resultado=$this->connection->ExecuteNonQuery($query);
+                    $parametersTam["tamano"] = $tamaño;
+                    $parametersTam["id_usuario"] = $_SESSION["UserId"];
+
+                    $resultado=$this->connection->ExecuteNonQuery($query, $parametersTam);
+
                 }
                 return $resultado;
     }
